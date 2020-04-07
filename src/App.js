14f9103e77e7f2787df9bdfe7e0bd9a6b0c77f44
kdm <kdm1jkmm@gmail.com>
2020-04-07 22:19:30 +0900

    영화 api 사용하기

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React from 'react';
+import axios from 'axios';
+import Movie from "./Movie";
 
-function Food({ fav }) {
-  return <h1>I like {fav}</h1>
-}
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
 
-function App() {
-  return (
-    <div>
-      <div>Hello!</div>
-      <Food fav="1" />
-    </div>
-  );
+  getMovies = async () => {
+    const { data: { data: { movies } } } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    this.setState({ isLoading: false, movies });
+  }
+
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  render() {
+    const { isLoading, movies } = this.state;
+    return <div>
+      {isLoading ? "Loading..." : movies.map(movie => (
+        <Movie key={movie.id} id={movie.id} year={movie.year} summary={movie.summary} title={movie.title} poster={movie.medium_cover_image} />
+      ))}
+    </div>;
+  }
 }
 
 export default App;
