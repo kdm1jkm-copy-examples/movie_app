d8f87ea8236b232e245158a1e6203d7216e36366
kdm <kdm1jkmm@gmail.com>
2020-04-07 22:16:50 +0900

    Updates

---
+++ precache-manifest.5b9f4d38b680bd747e659827ce10a80e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd8ef2a3c5d7e9fecf3ecba1cc7e67b5",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "17030ea61ab85dbbd31c",
+    "url": "/movie_app/static/js/2.d73c51c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.d73c51c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24f16c21bf61d1e3db6a",
+    "url": "/movie_app/static/js/main.dbf9635f.chunk.js"
+  },
+  {
+    "revision": "c4e503dc5fc5f4a3fb01",
+    "url": "/movie_app/static/js/runtime-main.202e151f.js"
+  }
+]);
